4e8255d4c2cdddef2b345ccc8adfbdee1adb0b76
Ved Pradhan <vedprad1@gmail.com>
2020-07-19 10:53:17 -0700

    Created views and url patterns for business.

--- business/urls.py
+++ business/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.index, name="customer"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register_view, name="register"),
+    path("edit", views.edit, name="edit"),
+    path("analytics", views.analytics, name="analytics"),
+    path("spots", views.spots, name="spots"),
+    path("positive", views.positive, name="positive"),
+    path("negative", views.negative, name="negative"),
+    path("alerts", views.alerts, name="alerts"),
 ]
--- business/views.py
+++ business/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pass
+
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
+
+def register_view(request):
+    pass
+
+def edit(request):
+    pass
+
+def analytics(request):
+    pass
+
+def spots(request):
+    pass
+
+def positive(request):
+    pass
+
+def negative(request):
+    pass
+
+def alerts(request):
+    pass
