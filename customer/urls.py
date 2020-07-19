820ff9c4e23e64811cda838b74ec19349ebe7087
Ved Pradhan <vedprad1@gmail.com>
2020-07-19 10:56:51 -0700

    Created views and url patterns for customer.

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.index, name="customer"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("signup", views.signup_view, name="signup"),
+    path("search", views.search, name="search"),
+    path("view", views.view, name="view"),
+    path("reccomendation", views.reccomendation, name="reccomendation"),
+    path("reserve", views.reserve, name="reserve"),
+    path("positive", views.positive, name="positive"),
+    path("negative", views.negative, name="negative"),
+    path("alerts", views.alerts, name="alerts"),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pass
+
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
+
+def signup_view(request):
+    pass
+
+def search(request):
+    pass
+
+def view(request):
+    pass
+
+def reccomendation(request):
+    pass
+
+def reserve(request):
+    pass
+
+def positive(request):
+    pass
+
+def negative(request):
+    pass
+
+def alerts(request):
+    pass
