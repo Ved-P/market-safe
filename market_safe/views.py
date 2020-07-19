410eeedbb8e092bed90e94981f4ac1abb1a02c87
Ved Pradhan <vedprad1@gmail.com>
2020-07-19 11:21:44 -0700

    Added templates directory with home.html.

--- market_safe/views.py
+++ market_safe/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Market Safe!")
+    return render(request, "market_safe/home.html")
