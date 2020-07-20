3e8a362a2f964433786e7b46e29d812b6557eadf
Ved Pradhan <vedprad1@gmail.com>
2020-07-20 16:08:10 -0700

    Create Customer and Business models.

--- business/models.py
+++ business/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Business(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="business")
+    name = models.CharField(max_length=150)
+    max_customers = models.IntegerField()
+    avg_customers = models.IntegerField()
+    employees = models.IntegerField()
+    area = models.IntegerField()
+    open = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer")
+    test_status = models.IntegerField()
+    quarantined = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
